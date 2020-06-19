23f6287f95298a8d0c1709529fbe9c3ef5fcb5ba
Tomas <tomascf123@gmail.com>
2020-06-18 19:47:53 -0500

    (Login temporal) Después de un largo día de estresarme por no saber flexbox :'D , logré hacer el primer prototipo del login de wrotter, aprendí varias cosas, como que en <Image/> si se usa "uri" en la prop source, se debe poner un tamaño por defecto, para que este no ponga probemas, además tambien aprendí que si se pone "flex:1" en un child, este perdera el flex del padre y no se dejará mover respecto a este, tambien me dí cuenta que en android 9 hay un bug y no se puede cambiar el color del blinking cursor

--- App.js
+++ App.js
@@ -1,19 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput,
+Button,
+ImageBackground} from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <ImageBackground style={styles.container}>
+      
+        
+      <View style={styles.logotipo}>
+        <Image 
+        source={ require("./assets/logoWrotter.png")} style={styles.logo}></Image>
+      </View>
+
+      <View style={styles.login}>
+            <TextInput 
+            selectionColor="red"
+            maxLenght={20}
+            placeholder="Usuario"
+            style={styles.textinputs}/>
+            <TextInput
+            selectionColor="red"
+            placeholder="Contraseña"
+            style={styles.textinputs}/>
+          <View style={styles.botones}>
+            <View style={styles.boton}>
+              <Button color="#434343" title="Iniciar sesión"/>
+            </View>
+            <View  style={styles.boton}>
+              <Button color="#434343" title="Registrarse"/>
+            </View>
+          </View>
+          
+        </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:"column",
   },
+  logo:{
+    marginBottom: 0,
+    width:400,
+    height: 200,
+    resizeMode: "contain",
+  },
+  inputs:{
+    alignContent: "center",
+    justifyContent: "center"
+  },  
+  textinputs:{
+    borderWidth:1,
+    padding:1,
+    marginTop: 10,
+    color: "black",
+    width:200
+  },
+  logotipo:{
+    flex:1,
+    alignItems: "center",
+    justifyContent: "flex-end"
+  },
+  login:{
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flex: 1,
+    marginTop: 15
+  },  
+  botones:{
+    flexDirection:"row",
+    marginTop: 10,
+       
+  },
+  boton:{
+    margin: 10,
+    color: "black"
+  }
 });
